5454eb4f20e9fb2fce85487b3c38aa9c7251905a
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-03 22:18:35 +0530

    DAY_01

---
+++ Level_01/Problem_01.py
@@ -0,0 +1,13 @@
+# Take a number and print whether it’s positive, negative, or zero
+
+num = int(input('enter a number'))
+
+if num>0 :
+    print(f"{num} is postive")
+elif num<0 :
+    print(f"{num} is Negative")
+elif num==0:
+    print(f"{num} is zero")
+else :
+    print("give me proper number")
+
