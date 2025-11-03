f2e4b636b67f59a2dc3204073e085dfeedb75979
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-03 23:59:29 +0530

    praticed example

---
+++ Level_01/Problem_11.py
@@ -0,0 +1,10 @@
+ch = input("enter a ch")
+
+if 'A'<= ch <='Z':
+    print("upper case")
+elif 'a'<= ch<='b':
+    print("lowercase")
+elif '0'<=ch<='9':
+    print("digit")
+else:
+    print("this is a speacial number ")
