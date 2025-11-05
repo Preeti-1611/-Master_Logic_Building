305a863633a3819869c3924aafe4ff4c04877760
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-05 23:04:51 +0530

    example praticed

---
+++ Level_02/Probelm_01.py
@@ -0,0 +1,9 @@
+# Take three sides and check if they form a valid triangle.
+a = input(" enter a degree of a ")
+b=input("enter a degree of b")
+c = input("enter a degree of c ") 
+if a+b+c == 180:
+ print(" this is a triangle")
+
+else :
+ print(" this is not a triangle")
