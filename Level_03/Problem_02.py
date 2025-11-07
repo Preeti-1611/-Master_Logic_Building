b948e9f408ee63d1622f209dba67936ffd894308
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-07 22:50:56 +0530

    large and small

---
+++ Level_03/Problem_02.py
@@ -0,0 +1,10 @@
+a = int(input("enter a number"))
+b = int(input("enter a number"))
+c = int(input("enter a number"))
+
+if b>c and b>a:
+    print("the middle is largest")
+elif b<a and b<c:
+    print("the middle is samllest")
+else :
+    print("niether")
