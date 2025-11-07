8921effe8f3cc9b65aa218a1517797b86e442123
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-07 22:42:04 +0530

    distinct example

---
+++ Level_03/Problem_01.py
@@ -0,0 +1,13 @@
+# distint number
+a = int(input("enter a number"))
+b = int(input("enter a number"))
+c = int(input("enter a number"))
+
+if a!=b and a!=c and b!=c :
+    print("the values are distinct")
+else:
+    print("they are not distinct")
+
+
+
+
