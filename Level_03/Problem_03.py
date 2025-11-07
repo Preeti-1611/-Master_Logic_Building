5bacc254fc0d8b3346d0da0e733d441b051e2983
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-07 23:04:51 +0530

    equal digit

---
+++ Level_03/Problem_03.py
@@ -0,0 +1,15 @@
+list = []
+
+
+for i in range(0,4):
+    num = int(input("enter a number"))
+    list.append(num)
+
+
+print("the 4 digit is ",list)
+
+if list[0] == list[3]:
+    print("the first and last digit are equal")
+else:
+    print("not equal")
+
