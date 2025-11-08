5cbe7c776c35d5648b2d6cf1d89f17d69813887e
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-08 19:41:19 +0530

    solved Probelm

---
+++ Level_03/Problem_08.py
@@ -0,0 +1,7 @@
+# Take two angles of a triangle and compute the third angle
+
+a= int(input("enter a first angle of the triangle"))
+b = int(input("enter the second angle of the triangle"))
+c = a+b
+d = 180-c
+print(" the remaining angle of the triangle is ", d)
