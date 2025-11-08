4bca38dffa1e7717c559a4f497f2c4f227e0566d
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-08 19:40:49 +0530

    solved Probelm

---
+++ Level_03/Problem_06.py
@@ -0,0 +1,13 @@
+x = float(input("enter the coordinate of x"))
+y = float(input("enter the coordinate of y"))
+
+if x>=0 and y>=0:
+    print("this is 1st Quadrant")
+elif x<0 and y>=0:
+    print("2nd quadrant")
+elif x<0 and y<0:
+    print("this is 3rd quadrant")
+else:
+
+   print("this is 4th quradrant")
+          
