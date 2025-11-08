8b43203ef085bdd32c4862ccc62526e56c256b86
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-08 22:43:21 +0530

    solved Probelm

--- Level_03/Problem_09.py
+++ Level_03/Problem_09.py
@@ -1,7 +1,23 @@
 # 10. Check whether a number is a perfect square (without using the square root function).
-num = int(input("enter the square number"))
+# num = int(input("enter the square number"))
 
-if num%2 ==0 :
-    print( "its a perfect square")
+# if num%2 ==0 :
+#     print( "its a perfect square")
+# else:
+#     print("not a perfect square")
+
+
+num = int(input("Enter a number: "))
+is_square = False
+
+for i in range(1, num + 1):
+    if i * i == num:
+        is_square = True
+        break
+    elif i * i > num:
+        break
+
+if is_square:
+    print(num, "is a perfect square.")
 else:
-    print("not a perfect square")
+    print(num, "is not a perfect square.")
