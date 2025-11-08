def87db9dcae64a8a903258fd8eb5bfdaa4c5ad8
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-08 22:43:48 +0530

    solved Probelm

--- Level_03/Problem_10.py
+++ Level_03/Problem_10.py
@@ -1,2 +1,12 @@
 # Check if an amount can be evenly divided into 2000, 500, and 100 currency notes
+amount = int(input("Enter the amount: "))
+
+if amount % 2000 == 0:
+    print("Can be evenly divided into ₹2000 notes.")
+elif amount % 500 == 0:
+    print("Can be evenly divided into ₹500 notes.")
+elif amount % 100 == 0:
+    print("Can be evenly divided into ₹100 notes.")
+else:
+    print("Cannot be evenly divided into 2000, 500, or 100 notes.")
 
