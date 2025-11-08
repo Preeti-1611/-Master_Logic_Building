0816f837158fdf84aec1143dc086040f25ab1341
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-08 19:41:03 +0530

    solved Probelm

---
+++ Level_03/Problem_07.py
@@ -0,0 +1,8 @@
+# Check if a number lies within the range [100, 999]
+
+num= int(input('enter the number'))
+
+if 100<=num<=999:
+    print("the number is in between the range")
+else:
+    print("not in the range")
