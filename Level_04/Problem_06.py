5a988be24e7259fb113ba825156dda290bc12e83
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-09 17:16:32 +0530

    solved Probelm

---
+++ Level_04/Problem_06.py
@@ -0,0 +1,12 @@
+# Take two numbers and check if both are positive and their sum is less than 100.
+
+a = int(input("enter a number"))
+b = int(input("neter a number"))
+
+c = a+b
+
+if a >0 and b>0 and  c<100:
+    print("yes correct")
+
+else:
+    print("not correct")
