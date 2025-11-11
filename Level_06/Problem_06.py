6d217c350fd74185df8fda83aa1d708e53c69d08
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-11 23:12:35 +0530

    solved Probelm

---
+++ Level_06/Problem_06.py
@@ -0,0 +1,10 @@
+# Print the sum of first n natural numbers
+
+n = int(input('enter a n numberi will give the sum of it'))
+sum = 0
+for i in range(0,n+1):
+       sum+=i
+
+
+print(sum)
+# range is always greater then the given rate
