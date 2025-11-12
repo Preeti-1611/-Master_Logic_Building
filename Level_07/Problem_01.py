3fea16388cecb929b934684cd12c56309ff89d85
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-12 18:08:07 +0530

    sloved Problem

---
+++ Level_07/Problem_01.py
@@ -0,0 +1,12 @@
+# Count the number of digits in a given number
+
+n = int(input('enter a number i will find the number of digit in that number'))
+count = 0
+for i in str(n):
+    count+=1
+
+print('the number of digit ',count)
+
+
+
+# print("Number of digits:", len(str(n)))
