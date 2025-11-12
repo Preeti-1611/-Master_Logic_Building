ab4a624a3a528dda12cf44d90b0b8e86f42530fb
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-12 18:08:33 +0530

    sloved Problem

---
+++ Level_07/Problem_02.py
@@ -0,0 +1,21 @@
+# Print the reverse of a given number
+# s = input('enter the name')
+# print(s[::-1])
+
+# floor
+print(12//10)
+import math
+print(math.floor(4.8))  # 4
+print(7 // 2)           # 3  (floor division)
+
+
+
+# n = int(input("Enter a number: "))
+# reverse = 0
+
+# while n > 0:
+#     digit = n % 10        # take last digit
+#     reverse = reverse * 10 + digit  # add it to reverse
+#     n = n // 10           # remove last digit
+
+# print("The reverse of the number is:", reverse)
