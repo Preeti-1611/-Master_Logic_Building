ad77c257562f4e22f1fbae1d82afb945a1f928b6
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-12 12:38:16 +0530

    sloved Problem

---
+++ Level_06/Problem_11.py
@@ -0,0 +1,13 @@
+# for i in range(1,10):
+# print(f"{i} =",'*')
+
+# for i in range(1,5):
+#     print('*',end=" ")
+#     for j in range(1,4):
+#         print('*',end =" ")
+#     print('')
+
+for i in range(1,5):
+    print('x',end =" ")
+    for i in range(i):
+        print('#')
