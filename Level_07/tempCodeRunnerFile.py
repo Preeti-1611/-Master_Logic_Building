37a3b23818b673b0bc89123476bb8bd981017760
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-13 22:07:31 +0530

    sloved Problem

---
+++ Level_07/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+What is the Fibonacci series?
+
+# It’s a sequence of numbers where each number is the sum of the previous two.
+
+# So it starts with:
+
+# 0, 1, 1, 2, 3, 5, 8, 13, ...
+
+
+# Here’s how it’s formed:
+
+# Start with 0 and 1
+
+# Then add the last two numbers to get the next one
+# (0 + 1 = 1)
+# (1 + 1 = 2)
+# (1 + 2 = 3)
+# (2 + 3 = 5) ... and so
