711a06cf1bb0b7ad25b10cde2fafc398dacbc8e3
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-16 23:27:53 +0530

    solved Problem

---
+++ Level_09/Problem_01.py
@@ -0,0 +1,13 @@
+# Print n Stars on Same Line
+
+# for i in range(5):
+#     print("*",end =" ")
+
+# for i in range(6):
+#     print("*")
+
+for i in range(7):
+    print("*")
+    for j in range(i):
+        print("*")
+    print("\n")
