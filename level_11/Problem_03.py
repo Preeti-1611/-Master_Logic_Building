bf41a3f9bd389acd70feac005597307838654b8f
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-21 23:40:35 +0530

    solved Problem

---
+++ level_11/Problem_03.py
@@ -0,0 +1,19 @@
+# s = input("Enter a string: ")
+# if s == s[::-1]:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+
+s = input("Enter a string: ")
+rev = ""
+
+for i in s:
+    rev = i + rev
+
+print("Reversed:", rev)
+
+if s == rev:
+    print("Palindrome")
+else:
+ 2   print("Not Palindrome")
