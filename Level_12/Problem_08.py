e902219fc97a05e0a0fc48a57ed9653b727d8b11
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-26 21:58:52 +0530

    arrays questions sloved

---
+++ Level_12/Problem_08.py
@@ -0,0 +1,9 @@
+
+# Find the index of the maximum element
+arr = [10, 25, 7, 30, 18]
+
+max_value = max(arr)
+max_index = arr.index(max_value)
+
+print("Maximum element:", max_value)
+print("Index of maximum element:", max_index)
