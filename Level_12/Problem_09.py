be2ebacad1cf651632200e8077086902d323a6d2
Preeti-1611 <preetinagarale16@gmail.com>
2025-11-26 21:57:40 +0530

    arrays questions sloved

---
+++ Level_12/Problem_09.py
@@ -0,0 +1,8 @@
+# Find the index of the minimum element
+arr = [10, 25, 7, 30, 18]
+
+min_value = min(arr)
+min_index = arr.index(min_value)
+
+print("Minimum element:", min_value)
+print("Index of minimum element:", min_index)
