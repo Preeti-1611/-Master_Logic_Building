9d309555909fca557a0ce1ba34765b570cae0f5f
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-01 23:58:09 +0530

    sloved question

---
+++ DSA_Q/prime_num.py
@@ -0,0 +1,16 @@
+# check if two strings are anagrams
+
+
+# check if number is prime
+
+n = int(input("Enter a number: "))
+
+if n <= 1:
+    print("Not a prime number")
+else:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
