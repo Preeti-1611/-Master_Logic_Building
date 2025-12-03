932df33cd9ca56700173e0ee77b86034a5a6fb74
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-03 23:25:01 +0530

    sloved question

--- DSA_Q/factorial_num.py
+++ DSA_Q/factorial_num.py
@@ -7,3 +7,25 @@ fact = 1
 for i in range(1,n+1):
     fact*=i
 print(f"the factorial of the {n}  is = ",fact)
+
+
+n = int(input("Enter a number: "))
+fact = 1
+i = 1
+
+while i <= n:
+    fact = fact * i
+    i += 1
+
+print("Factorial =", fact)
+
+
+def factorial(n):
+    if n == 0 or n == 1:   # base condition
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+num = int(input("Enter a number: "))
+print("Factorial =", factorial(num))
+
