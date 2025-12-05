2f2eab115e64a406d1bb2400f4e23a4cdb966d84
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-05 23:09:50 +0530

    solved Problem

---
+++ Level_12/Problem_12.py
@@ -0,0 +1,7 @@
+arr = [1, 3, 5, 7, 9]
+x = int(input("Enter element to search: "))
+
+if x in arr:
+    print("Element exists in the array.")
+else:
+    print("Element does NOT exist in the array.")
