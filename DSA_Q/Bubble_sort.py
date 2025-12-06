c34baad55fb0e767aa5354ec567cae1723481f09
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-06 22:13:34 +0530

    solved Problem

--- DSA_Q/Bubble_sort.py
+++ DSA_Q/Bubble_sort.py
@@ -1,8 +1,15 @@
-arr = list(map(int, input("Enter numbers: ").split()))
+#bubble sort
+n=int(input("entera number"))
+arr=[]
 
-for i in range(len(arr)):
-    for j in range(0, len(arr) - i - 1):
-        if arr[j] > arr[j + 1]:
-            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+for i in range(n):
+    n=int(input('enter a number'))
+    arr.append(n)
+print("the array is",arr)
 
-print("Sorted array:", arr)
+for i in range(len(arr)-1):
+    for j in range(len(arr)-i-1):
+        if arr[j]>arr[j+1]:
+          arr[j],arr[j+1]=arr[j+1],arr[j]
+
+print(arr)
