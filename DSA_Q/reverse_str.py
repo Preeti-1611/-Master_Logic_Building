e01c3360868c6c16db26127c39eec638eadc7c3a
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-06 22:14:57 +0530

    solved Problem

--- DSA_Q/reverse_str.py
+++ DSA_Q/reverse_str.py
@@ -1,4 +1,17 @@
 #reverse string
 
-s="PreetiNagarale"
-print(s[::-1]) 
+# s="PreetiNagarale"
+# print(s[::-1]) 
+
+a1 = 'HELLO'
+r = ""
+for i in a1:
+    r=i+r
+print(r)
+
+
+# #deseceding order
+# arr=[3,44,3,25,6,4,54,3]
+# print(set(arr))
+# arr.sort( reverse = True)
+# print(arr)
