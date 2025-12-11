ec8fdc257f8f6278dbd168c70ad8c6ddacf3066b
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-11 22:05:09 +0530

    solved Problem

--- DSA_Q/Bubble_sort.py
+++ DSA_Q/Bubble_sort.py
@@ -1,12 +1,13 @@
 #bubble sort
-n=int(input("entera number"))
-arr=[]
+# n=int(input("entera number"))
+# arr=[]
 
-for i in range(n):
-    n=int(input('enter a number'))
-    arr.append(n)
-print("the array is",arr)
+# for i in range(n):
+#     n=int(input('enter a number'))
+#     arr.append(n)
+# print("the array is",arr)
 
+arr = [4,62,6,3,7,9]
 for i in range(len(arr)-1):
     for j in range(len(arr)-i-1):
         if arr[j]>arr[j+1]:
