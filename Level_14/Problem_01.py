31e49034180327f4689478ad971de7c0e0694f5c
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-18 23:58:41 +0530

    solved Problem

---
+++ Level_14/Problem_01.py
@@ -0,0 +1,11 @@
+# Input an element x — check if it exists in the array.
+
+
+x = int(input("Enter a element"))
+
+arr = [4,5,7,3,6,-2,4]
+
+if x in arr:
+    print("yes the given element exist in the array")
+else:
+    print("Not exist")
