c76baa1d3e4384d088a1338c7885736756709bb0
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-22 22:36:47 +0530

    solved Problem

--- DSA_Q/linear_search.py
+++ DSA_Q/linear_search.py
@@ -1,5 +1,26 @@
-arr = [1,4,7,9]
-x = 7
+arr = [10, 25, 30, 45, 60]
+key = 45
+
+found = False
+
 for i in range(len(arr)):
-    if arr[i] == x:
-        print("Found at index", i)
+    if arr[i] == key:
+        print("Element found at index", i)
+        found = True
+        break
+
+if not found:
+    print("Element not found")
+
+
+
+
+arr = [10, 25, 30, 45, 60]
+key = 45
+
+for i in arr:
+    if i == key:
+        print("Element found")
+        break
+else:
+    print("Not found")
