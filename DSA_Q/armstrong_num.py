4c2b5fafdd7bcff94365f66e26a7ced4b7b359d7
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-22 22:38:20 +0530

    solved Problem

--- DSA_Q/armstrong_num.py
+++ DSA_Q/armstrong_num.py
@@ -10,3 +10,30 @@ ans = 0
 for i in str(n):
     ans +=int(i)**count
 print(ans)
+
+
+if ans == n:
+    print("This is armstrong numner")
+else:
+    print("not an armstrong number")
+
+
+
+
+
+
+# why we use temp here
+num = int(input("Enter a number: "))
+temp = num
+n = len(str(num))
+sum = 0
+
+while temp > 0:  #bcz when num>0 means after iteration it will become 0
+    digit = temp % 10
+    sum += digit ** n
+    temp //= 10
+
+if sum == num:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
