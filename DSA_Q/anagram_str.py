950ffaba97d34ad4b0bde147c447b25342494664
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-22 22:37:55 +0530

    solved Problem

--- DSA_Q/anagram_str.py
+++ DSA_Q/anagram_str.py
@@ -5,3 +5,13 @@ if sorted(s1) == sorted(s2):
     print("Yes, the strings are anagrams.")
 else:
     print("No, the strings are not anagrams.")
+
+
+# if s1.sort() == s2.sort():
+# s1 and s2 are strings
+
+# Strings do NOT have sort() method
+
+# sort() works only on lists
+# sorted() works on strings and returns a list.
+# 
