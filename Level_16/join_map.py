637ddd490e8740e9a924b1cd2bb851ee7617da4c
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-27 23:46:51 +0530

    solved Problem

---
+++ Level_16/join_map.py
@@ -0,0 +1,18 @@
+# integer to str
+
+lst = [3,6,7,3]
+string  = ''.join(map(str,lst))
+print(string[0])
+
+
+#number to str
+
+lst = ['4','8','2']
+string = ''.join(lst)
+print(string)
+
+
+lst = ['Preeti','adarsh']
+string = ''.join(map(str.upper ,lst))
+print(string)
+
