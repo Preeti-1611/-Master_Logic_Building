185a08980789faa69325903410460a970db8c451
Preeti-1611 <preetinagarale16@gmail.com>
2025-12-31 16:50:35 +0530

    solved Problem

--- DSA_Q/count_freq_arr.py
+++ DSA_Q/count_freq_arr.py
@@ -1,16 +1,29 @@
 #count the fequency of element in an array
 
-arr = [2, 3, 2, 5, 3, 2]
+# arr = [2, 3, 2, 5, 3, 2]
 
-frequency = {}       # empty dictionary to store counts
+# frequency = {}       # empty dictionary to store counts
 
-for num in arr:
-    if num in frequency:
-        frequency[num] += 1   # increase the count
-    else:
-        frequency[num] = 1    # add number with count 1
+# for num in arr:
+#     if num in frequency:
+#         frequency[num] += 1   # increase the count
+#     else:
+#         frequency[num] = 1    # add number with count 1
 
-print(frequency)
+# print(frequency)
+
+# using hashmap
+
+num = [3,4,2,3,3,8,6,4]
+
+hash_map = {}
+n = len(num)
+for i in range(0,n):
+    hash_map[num[i]] = hash_map.get(num[i],0)+1
+
+print(hash_map)
+
+
 
 
 
